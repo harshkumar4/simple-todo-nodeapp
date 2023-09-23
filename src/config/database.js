7d9d8a63aad6b6a2b56099bf7a83ca265c14df0b
harsh-scrobits <harshkumar.scrobits@gmail.com>
2023-09-23 15:04:36 +0530

    added dotenv and env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
 const mysql2 = require('mysql2');
 
+const env = process.env;
+
 
 const pool = mysql2.createPool({
-    host: 'localhost',
-    database:'todo-app',
-    user:'root',
-    password:'1234@sql',
+    host: env.HOST,
+    database: env.DATABASE,
+    user: env.USER,
+    password: env.PASSWORD,
 });
 
 module.exports = pool.promise()
